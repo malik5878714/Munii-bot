ac0aa9f49b681d10264198e126bd98f27fd0054c
malik5878714 <malik5878714@gmail.com>
2026-09-05 21:22:43 +0500

    Create women.js

---
+++ Ayush/commonds/women.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "women",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "𝐄𝐌𝐌𝐀 𝐎𝐋𝐈𝐕𝐈𝐀", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "women",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("Women")==0 || event.body.indexOf("women")==0 || event.body.indexOf("WOMEN")==0 || event.body.indexOf("☕")==0) {
+		var msg = {
+				body: "hahaha Women 🤣",
+				attachment: fs.createReadStream(__dirname + `/noprefix/wn.mp4`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("☕", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
